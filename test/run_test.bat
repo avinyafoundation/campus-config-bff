934b634c02b4865623eb0884afe57c6c05f7041a
samisa-abeysinghe <35890847+samisa-abeysinghe@users.noreply.github.com>
2023-01-09 15:24:39 +0530

    Update run_test.bat

--- test/run_test.bat
+++ test/run_test.bat
@@ -2,7 +2,7 @@ curl -X POST -H "Content-Type: application/json" -d @avinya_type.json http://loc
 
 curl http://localhost:9090/avinya_types
 
-curl -X PUT -H "Content-Type: application/json" -d @avinya_type.jsonn http://localhost:9090/avinya_types 
+curl -X PUT -H "Content-Type: application/json" -d @avinya_type.json http://localhost:9090/avinya_types 
 
 curl http://localhost:9090/avinya_types
 
